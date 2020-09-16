612174339ac39a50f017c5a427dd6921869a861b
Tim Knell <timknell@gmail.com>
2020-09-15 21:49:06 -0700

    first commit

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public string username;
+    public bool human;
+    public HUD hud;
+    public WorldObject SelectedObject {get; set;}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hud = GetComponentInChildren< HUD >();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/WorldObject/Building/Building.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : WorldObject
+{
+    protected override void Awake() {
+        base.Awake();
+    }
+        
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void OnGUI() => base.OnGUI();
+}
